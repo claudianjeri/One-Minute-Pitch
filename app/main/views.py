aa422c64acf7fe7e9d6cd2ef94ec6e56e6102b81
claudianjeri <claudianjeri04@gmail.com>
2018-05-23 16:58:30 +0300

    setting up form features

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,24 @@
 from . import main
-from flask import render_template 
+from flask import render_template, request, redirect, url_for
 from ..models import Pitch
 from .forms import PitchForm
 
+Pitch =pitch.Pitch
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/')
+def create(usname):
+
+    form = PitchForm()
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch_data = form.pitch.data
+        new_pitch = Pitch(category, pitch_data)
+        new_pitch.save_pitch()
+
+    pitch = Pitch.get_pitch(usname)
+
+return render_template('new.html',pitch_form = form,usname = usname)
