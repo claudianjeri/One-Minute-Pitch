559a8d9acfb7c05212961cf2862e18e5e0669d8c
claudianjeri <claudianjeri04@gmail.com>
2018-05-23 07:28:58 +0300

    added input field for new pitches

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm #import FlaskForm class from the module
+from wtforms import StringField, TextAreaField, SubmitField #import these to help in creation of a text area field and submit button.
+from wtforms.validators import Required #import required that will prevent submission if an input field is empty
+
+
+class PitchForm(FlaskForm): #create a class that inherits from FlaskForm class
+    category = StringField('Pitch Category', validators=[Required()])
+    pitch = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
