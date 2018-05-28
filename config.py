ee26a96a9e32c3dd668d4a1bf8cf4a8c67d082a3
claudianjeri <claudianjeri04@gmail.com>
2018-05-29 02:03:33 +0300

    Initial Migration

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njeri:1997@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -9,7 +9,7 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njeri:1997@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njeri:1997@localhost/pitches'                                                                                                                                                                                                                                                                                                                                                                                                      
     DEBUG = True
 
 config_options ={"production":ProdConfig,"default":DevConfig}
