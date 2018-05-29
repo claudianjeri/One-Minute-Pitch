0ea9cd248e59a91f45b4f04848a8cac962def573
claudianjeri <claudianjeri04@gmail.com>
2018-05-29 10:33:53 +0300

    added log in and registration forms

--- app/models.py
+++ app/models.py
@@ -2,8 +2,9 @@ from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash #generate takes in a password and return  hash and check does the opposite.
 from flask_login import UserMixin
 
-@login_manager.user_loaderdef load_user(user_id):
-return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
